f47fe90a0cf508e5dca6aca8212efcc459506cb8
Blue <bluestorm.android@gmail.com>
2018-11-06 00:15:34 +0100

    Basic Constants

---
+++ ProjectDMG/Utils/Constants.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectDMG.Utils {
+    public static class Constants {
+        public const int DMG_4Mhz = 4194304;
+        public const int _60hz = 60;
+        public const int CYCLES_PER_UPDATE = DMG_4Mhz / _60hz;
+    }
+}
