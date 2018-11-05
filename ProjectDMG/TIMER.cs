4efff06316b6e9dad6f15c64654ce7523e163519
Blue <bluestorm.android@gmail.com>
2018-11-06 00:15:54 +0100

    Basic Barebone

---
+++ ProjectDMG/TIMER.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ProjectDMG {
+    internal class TIMER {
+        public TIMER() {
+        }
+
+        internal void update(int cycles, MMU mmu) {
+            //throw new NotImplementedException();
+        }
+    }
+}
