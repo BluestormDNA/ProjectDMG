eee5144eb320bd566685d5fa6e330e1e24710cdc
Blue <bluestorm.android@gmail.com>
2018-11-14 01:42:18 +0100

    Another rewrite of ppu update...

--- ProjectDMG/DMG/PPU.cs
+++ ProjectDMG/DMG/PPU.cs
@@ -24,83 +24,90 @@ namespace ProjectDMG {
 
         public void update(int cycles, MMU mmu) {
             scanlineCounter += cycles;
-
             byte currentMode = (byte)(mmu.STAT & 0b00000011);
+            //Console.WriteLine("Update PPU STAT:" + mmu.STAT.ToString("x2") + " " + currentMode) ;
 
-            if (isLCDEnabled(mmu)) {
-                switch (currentMode) {
-                    case 2: //Accessing OAM - Mode 2 (80 cycles)
-                        if (scanlineCounter >= OAM_CYCLES) {
-                            changeSTATMode(3, mmu);
-                            scanlineCounter = 0;
-                        }
-                        break;
-                    case 3: //Accessing VRAM - Mode 3 (172 cycles) Total M2+M3 = 252 Cycles
-                        if (scanlineCounter >= VRAM_CYCLES) {
-                            changeSTATMode(0, mmu);
-                            drawScanLine(mmu);
-                            scanlineCounter = 0;
-                        }
-                        break;
-                    case 0: //HBLANK - Mode 0 (204 cycles) Total M2+M3+M0 = 456 Cycles
-                        if (scanlineCounter >= HBLANK_CYCLES) {
-                            mmu.LY++;
-                            scanlineCounter = 0;
-                            
-                            if(mmu.LY == SCREEN_HEIGHT) { //check if we arrived Vblank
-                                changeSTATMode(1, mmu);
-                                //we should draw frame here
-                            } else { //not arrived yet so return to 2
-                                changeSTATMode(2, mmu);
-                            }
-                        }
-                        break;
-                    case 1: //VBLANK - Mode 1 (4560 cycles - 10 lines)
-                        if (scanlineCounter >= SCANLINE_CYCLES) {
-                            mmu.LY++;
-                            scanlineCounter = 0;
+            //if (isLCDEnabled(mmu)) {
+            switch (currentMode) {
+                case 2: //Accessing OAM - Mode 2 (80 cycles)
+                    if (scanlineCounter >= OAM_CYCLES) {
+                        changeSTATMode(3, mmu);
+                        scanlineCounter = 0;
+                        //Console.WriteLine("Update PPU INSIDE OAM");
+                    }
+                    break;
+                case 3: //Accessing VRAM - Mode 3 (172 cycles) Total M2+M3 = 252 Cycles
+                    if (scanlineCounter >= VRAM_CYCLES) {
+                        changeSTATMode(0, mmu);
+                        drawScanLine(mmu);
+                        scanlineCounter = 0;
+                       // Console.WriteLine("Update PPU INSIDE VRAM");
+                    }
+                    break;
+                case 0: //HBLANK - Mode 0 (204 cycles) Total M2+M3+M0 = 456 Cycles
+                    if (scanlineCounter >= HBLANK_CYCLES) {
+                        //Console.WriteLine("Update PPU INSIDE HBLANK" + mmu.LY);
+                        //mmu.debugIO();
 
-                            if (mmu.LY > SCREEN_VBLANK_HEIGHT) { //check end of VBLANK
-                                changeSTATMode(2, mmu);
-                                mmu.LY = 0;
-                            }
+                        mmu.LY++;
+                        scanlineCounter = 0;
+
+                        if (mmu.LY == SCREEN_HEIGHT) { //check if we arrived Vblank
+                            changeSTATMode(1, mmu);
+                            //we should draw frame here
+                        } else { //not arrived yet so return to 2
+                            changeSTATMode(2, mmu);
                         }
-                        break;
-                }
-            } else { //LCD Disabled
-                scanlineCounter = 0;
-                mmu.LY = 0;
-                mmu.STAT = (byte)(mmu.STAT & ~0b11111100);
+                    }
+                    break;
+                case 1: //VBLANK - Mode 1 (4560 cycles - 10 lines)
+                    if (scanlineCounter >= SCANLINE_CYCLES) {
+                        mmu.LY++;
+                        scanlineCounter = 0;
+
+                        //Console.WriteLine("Update PPU INSIDE VBLANK");
+
+                        if (mmu.LY > SCREEN_VBLANK_HEIGHT) { //check end of VBLANK
+                            changeSTATMode(2, mmu);
+                            mmu.LY = 0;
+                        }
+                    }
+                    break;
             }
+            //} else { //LCD Disabled
+            //    scanlineCounter = 0;
+            //    mmu.LY = 0;
+            //    mmu.STAT = (byte)(mmu.STAT & ~0b11111100);
+            //}
         }
 
         private void changeSTATMode(int mode, MMU mmu) {
             switch (mode) {
                 case 2: //Accessing OAM - Mode 2 (80 cycles)
-                    mmu.bitSet(1, mmu.STAT);
-                    mmu.bitClear(0, mmu.STAT);
+                    mmu.STAT = mmu.bitSet(1, mmu.STAT);
+                    mmu.STAT = mmu.bitClear(0, mmu.STAT);
                     if (mmu.isBit(5, mmu.STAT)) { // Bit 5 - Mode 2 OAM Interrupt         (1=Enable) (Read/Write)
                         requestPPUInterrupt(5);
-                    } 
+                    }
+                    //Console.WriteLine("Inside Changestat 2:" + mmu.STAT.ToString("x2"));
                     break;
                 case 3: //Accessing VRAM - Mode 3 (172 cycles) Total M2+M3 = 252 Cycles
-                    mmu.bitSet(1, mmu.STAT);
-                    mmu.bitSet(0, mmu.STAT);
+                    mmu.STAT = mmu.bitSet(1, mmu.STAT);
+                    mmu.STAT = mmu.bitSet(0, mmu.STAT);
                     break;
                 case 0: //HBLANK - Mode 0 (204 cycles) Total M2+M3+M0 = 456 Cycles
-                    mmu.bitClear(1, mmu.STAT);
-                    mmu.bitClear(0, mmu.STAT);
+                    mmu.STAT = mmu.bitClear(1, mmu.STAT);
+                    mmu.STAT = mmu.bitClear(0, mmu.STAT);
                     if (mmu.isBit(3, mmu.STAT)) { // Bit 3 - Mode 0 H-Blank Interrupt     (1=Enable) (Read/Write)
                         requestPPUInterrupt(3);
                     }
                     break;
                 case 1: //VBLANK - Mode 1 (4560 cycles - 10 lines)
-
-                    mmu.bitClear(1, mmu.STAT);
-                    mmu.bitSet(0, mmu.STAT);
+                    mmu.STAT = mmu.bitClear(1, mmu.STAT);
+                    mmu.STAT = mmu.bitSet(0, mmu.STAT);
                     if (mmu.isBit(4, mmu.STAT)) { // Bit 4 - Mode 1 V-Blank Interrupt     (1=Enable) (Read/Write)
                         requestPPUInterrupt(4);
-                    } 
+                    }
                     break;
             }
         }
