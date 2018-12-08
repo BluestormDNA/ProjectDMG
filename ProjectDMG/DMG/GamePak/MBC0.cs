58cb9860b5ec0f608b3c44caa2483bfcc8090217
Blue <bluestorm.android@gmail.com>
2018-12-09 00:02:12 +0100

    Added MBC0 support

---
+++ ProjectDMG/DMG/GamePak/MBC0.cs
@@ -0,0 +1,18 @@
+﻿namespace ProjectDMG.DMG.GamePak {
+    class MBC0 : IGamePak {
+
+        byte[] ROM;
+
+        public void Init(byte[] ROM) {
+            this.ROM = ROM;
+        }
+
+        public byte ReadByte(ushort addr) {
+            return ROM[addr];
+        }
+
+        public void WriteByte(ushort addr, byte value) {
+            //ROM[addr] = value; //MBC0 should ignore writes
+        }
+    }
+}
