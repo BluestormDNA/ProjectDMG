dbb32b5228e476334b4e818d26cfcfe21a09ea85
Blue <bluestorm.android@gmail.com>
2019-06-17 11:20:19 +0200

    Removed BootRom req. Added default 0x100 values. Simplified switch statements.

--- ProjectDMG/DMG/MMU.cs
+++ ProjectDMG/DMG/MMU.cs
@@ -1,6 +1,7 @@
 ﻿using ProjectDMG.DMG.GamePak;
 using System;
 using System.IO;
+using System.Runtime.CompilerServices;
 
 namespace ProjectDMG {
     public class MMU {
@@ -8,24 +9,19 @@ namespace ProjectDMG {
         //GamePak
         private IGamePak gamePak;
 
-        //BootRom
-        private byte[] BOOT_ROM = new byte[0x100];
         //DMG Memory Map
         private byte[] VRAM = new byte[0x2000];
         private byte[] WRAM0 = new byte[0x1000];
         private byte[] WRAM1 = new byte[0x1000];
         private byte[] OAM = new byte[0xA0];
-        public byte[] IO = new byte[0x80];
-        private byte[] HRAM = new byte[0x7F];
-
-        //BootRom Reg
-        public byte BR { get { return IO[0x50]; } set { IO[0x50] = value; } }
+        private byte[] IO = new byte[0x80];
+        private byte[] HRAM = new byte[0x80];
 
         //Timer IO Regs
         public byte DIV { get { return IO[0x04]; } set { IO[0x04] = value; } } //FF04 - DIV - Divider Register (R/W)
         public byte TIMA { get { return IO[0x05]; } set { IO[0x05] = value; } } //FF05 - TIMA - Timer counter (R/W)
         public byte TMA { get { return IO[0x06]; } set { IO[0x06] = value; } } //FF06 - TMA - Timer Modulo (R/W)
-        public byte TAC { get { return IO[0x07]; } set { IO[0x07] =  value; } } //FF07 - TAC - Timer Control (R/W)
+        public byte TAC { get { return IO[0x07]; } set { IO[0x07] = value; } } //FF07 - TAC - Timer Control (R/W)
         public bool TAC_ENABLED { get { return (IO[0x07] & 0x4) != 0; } } // Check if byte 2 is 1
         public byte TAC_FREQ { get { return (byte)(IO[0x07] & 0x3); } } // returns byte 0 and 1
 
@@ -35,7 +31,7 @@ namespace ProjectDMG {
         //Bit 2: Timer Interrupt Enable(INT 50h)  (1=Enable)
         //Bit 3: Serial Interrupt Enable(INT 58h)  (1=Enable)
         //Bit 4: Joypad Interrupt Enable(INT 60h)  (1=Enable)
-        public byte IE { get; set; }//FFFF - IE - Interrupt Enable (R/W)
+        public byte IE { get { return HRAM[0x7F]; } set { HRAM[0x7F] = value; } }//FFFF - IE - Interrupt Enable (R/W)
         public byte IF { get { return IO[0x0F]; } set { IO[0x0F] = value; } }//FF0F - IF - Interrupt Flag (R/W)
 
         //PPU IO Regs
@@ -61,87 +57,104 @@ namespace ProjectDMG {
             //FF4D - KEY1 - CGB Mode Only - Prepare Speed Switch
             //HardCoded to FF to identify DMG as 00 is GBC
             IO[0x4D] = 0xFF;
+
+            IO[0x10] = 0x80;
+            IO[0x11] = 0xBF;
+            IO[0x12] = 0xF3;
+            IO[0x14] = 0xBF;
+            IO[0x16] = 0x3F;
+            IO[0x19] = 0xBF;
+            IO[0x1A] = 0x7F;
+            IO[0x1B] = 0xFF;
+            IO[0x1C] = 0x9F;
+            IO[0x1E] = 0xBF;
+            IO[0x20] = 0xFF;
+            IO[0x23] = 0xBF;
+            IO[0x24] = 0x77;
+            IO[0x25] = 0xF3;
+            IO[0x26] = 0xF1;
+            IO[0x40] = 0x91;
+            IO[0x47] = 0xFC;
+            IO[0x48] = 0xFF;
+            IO[0x49] = 0xFF;
         }
 
         public byte readByte(ushort addr) {
-            switch (addr) {                                             // General Memory Map 64KB
-                case ushort r when addr >= 0x0000 && addr <= 0x3FFF:    //0000-3FFF 16KB ROM Bank 00 (in cartridge, private at bank 00)
-                    if (BR == 0 && addr < 0x100) {
-                        return BOOT_ROM[addr];
-                    } else {
-                        return gamePak.ReadLoROM(addr);
-                    }
-                case ushort r when addr >= 0x4000 && addr <= 0x7FFF:    // 4000-7FFF 16KB ROM Bank 01..NN(in cartridge, switchable bank number)
-                    return gamePak.ReadHiROM((ushort)(addr - 0x4000));
-                case ushort r when addr >= 0x8000 && addr <= 0x9FFF:    // 8000-9FFF 8KB Video RAM(VRAM)(switchable bank 0-1 in CGB Mode)
-                    return VRAM[addr - 0x8000];
-                case ushort r when addr >= 0xA000 && addr <= 0xBFFF:    // A000-BFFF 8KB External RAM(in cartridge, switchable bank, if any)
-                    return gamePak.ReadERAM((ushort)(addr - 0xA000));
-                case ushort r when addr >= 0xC000 && addr <= 0xCFFF:    // C000-CFFF 4KB Work RAM Bank 0(WRAM) <br/>
-                case ushort s when addr >= 0xE000 && addr <= 0xEFFF:    // E000-FDFF Same as 0xC000-DDFF(ECHO)  
+            switch (addr) {                           // General Memory Map 64KB
+                case ushort _ when addr <= 0x3FFF:    //0000-3FFF 16KB ROM Bank 00 (in cartridge, private at bank 00)
+                    return gamePak.ReadLoROM(addr);
+                case ushort _ when addr <= 0x7FFF:    // 4000-7FFF 16KB ROM Bank 01..NN(in cartridge, switchable bank number)
+                    return gamePak.ReadHiROM(addr);
+                case ushort _ when addr <= 0x9FFF:    // 8000-9FFF 8KB Video RAM(VRAM)(switchable bank 0-1 in CGB Mode)
+                    return VRAM[addr & 0x1FFF];
+                case ushort _ when addr <= 0xBFFF:    // A000-BFFF 8KB External RAM(in cartridge, switchable bank, if any)
+                    return gamePak.ReadERAM(addr);
+                case ushort _ when addr <= 0xCFFF:    // C000-CFFF 4KB Work RAM Bank 0(WRAM) <br/>
                     return WRAM0[addr & 0xFFF];
-                case ushort r when addr >= 0xD000 && addr <= 0xDFFF:    // D000-DFFF 4KB Work RAM Bank 1(WRAM)(switchable bank 1-7 in CGB Mode) <br/>
-                case ushort s when addr >= 0xF000 && addr <= 0xFDFF:    // E000-FDFF Same as 0xC000-DDFF(ECHO)
+                case ushort _ when addr <= 0xDFFF:    // D000-DFFF 4KB Work RAM Bank 1(WRAM)(switchable bank 1-7 in CGB Mode) <br/>
                     return WRAM1[addr & 0xFFF];
-                case ushort r when addr >= 0xFE00 && addr <= 0xFE9F:    // FE00-FE9F Sprite Attribute Table(OAM)
+                case ushort _ when addr <= 0xEFFF:    // E000-FDFF Same as 0xC000-DDFF(ECHO)  
+                    return WRAM0[addr & 0xFFF];
+                case ushort _ when addr <= 0xFDFF:    // E000-FDFF Same as 0xC000-DDFF(ECHO)
+                    return WRAM1[addr & 0xFFF];
+                case ushort _ when addr <= 0xFE9F:    // FE00-FE9F Sprite Attribute Table(OAM)
                     return OAM[addr - 0xFE00];
-                case ushort r when addr >= 0xFEA0 && addr <= 0xFEFF:    // FEA0-FEFF Not Usable 0
+                case ushort _ when addr <= 0xFEFF:    // FEA0-FEFF Not Usable 0
                     return 0x00;
-                case ushort r when addr >= 0xFF00 && addr <= 0xFF7F:    // FF00-FF7F IO Ports
-                    return IO[addr - 0xFF00];
-                case ushort r when addr >= 0xFF80 && addr <= 0xFFFE:    // FF80-FFFE High RAM(HRAM)
-                    return HRAM[addr - 0xFF80];
-                case 0xFFFF:                                            // FFFF Interrupt Enable Register
-                    return IE;
+                case ushort _ when addr <= 0xFF7F:    // FF00-FF7F IO Ports
+                    return IO[addr & 0x7F];
+                case ushort _ when addr <= 0xFFFF:    // FF80-FFFE High RAM(HRAM)
+                    return HRAM[addr & 0x7F];
                 default:
                     return 0xFF;
             }
         }
 
         public void writeByte(ushort addr, byte b) {
-            switch (addr) {                                              // General Memory Map 64KB
-                case ushort r when addr >= 0x0000 && addr <= 0x7FFF:     //0000-3FFF 16KB ROM Bank 00 (in cartridge, private at bank 00) 4000-7FFF 16KB ROM Bank 01..NN(in cartridge, switchable bank number)
+            switch (addr) {                            // General Memory Map 64KB
+                case ushort _ when addr <= 0x7FFF:     //0000-3FFF 16KB ROM Bank 00 (in cartridge, private at bank 00) 4000-7FFF 16KB ROM Bank 01..NN(in cartridge, switchable bank number)
                     gamePak.WriteROM(addr, b);
                     break;
-                case ushort r when addr >= 0x8000 && addr <= 0x9FFF:    // 8000-9FFF 8KB Video RAM(VRAM)(switchable bank 0-1 in CGB Mode)
-                    VRAM[addr - 0x8000] = b;
+                case ushort _ when addr <= 0x9FFF:    // 8000-9FFF 8KB Video RAM(VRAM)(switchable bank 0-1 in CGB Mode)
+                    VRAM[addr & 0x1FFF] = b;
                     break;
-                case ushort r when addr >= 0xA000 && addr <= 0xBFFF:    // A000-BFFF 8KB External RAM(in cartridge, switchable bank, if any) <br/>
-                    gamePak.WriteERAM((ushort)(addr - 0xA000), b);
+                case ushort _ when addr <= 0xBFFF:    // A000-BFFF 8KB External RAM(in cartridge, switchable bank, if any)
+                    gamePak.WriteERAM((ushort)(addr & 0x1FFF), b);
                     break;
-                case ushort r when addr >= 0xC000 && addr <= 0xCFFF:    // C000-CFFF 4KB Work RAM Bank 0(WRAM) <br/>
-                case ushort s when addr >= 0xE000 && addr <= 0xEFFF:    // E000-FDFF Same as 0xC000-DDFF(ECHO)  
+                case ushort _ when addr <= 0xCFFF:    // C000-CFFF 4KB Work RAM Bank 0(WRAM) <br/>
                     WRAM0[addr & 0xFFF] = b;
                     break;
-                case ushort r when addr >= 0xD000 && addr <= 0xDFFF:    // D000-DFFF 4KB Work RAM Bank 1(WRAM)(switchable bank 1-7 in CGB Mode) <br/>
-                case ushort s when addr >= 0xF000 && addr <= 0xFDFF:    // E000-FDFF Same as 0xC000-DDFF(ECHO)
+                case ushort _ when addr <= 0xDFFF:    // D000-DFFF 4KB Work RAM Bank 1(WRAM)(switchable bank 1-7 in CGB Mode)
                     WRAM1[addr & 0xFFF] = b;
                     break;
-                case ushort r when addr >= 0xFE00 && addr <= 0xFE9F:    // FE00-FE9F Sprite Attribute Table(OAM)
-                    OAM[addr - 0xFE00] = b;
+                case ushort _ when addr <= 0xEFFF:    // E000-FDFF Same as 0xC000-DDFF(ECHO)  
+                    WRAM0[addr & 0xFFF] = b;
                     break;
-                case ushort r when addr >= 0xFEA0 && addr <= 0xFEFF:    // FEA0-FEFF Not Usable
+                case ushort _ when addr <= 0xFDFF:    // E000-FDFF Same as 0xC000-DDFF(ECHO)
+                    WRAM1[addr & 0xFFF] = b;
+                    break;
+                case ushort _ when addr <= 0xFE9F:    // FE00-FE9F Sprite Attribute Table(OAM)
+                    OAM[addr & 0x9F] = b;
+                    break;
+                case ushort _ when addr <= 0xFEFF:    // FEA0-FEFF Not Usable
                     //Console.WriteLine("Warning: Tried to write to NOT USABLE space");
                     break;
-                case ushort r when addr >= 0xFF00 && addr <= 0xFF7F:    // FF00-FF7F IO Ports
+                case ushort _ when addr <= 0xFF7F:    // FF00-FF7F IO Ports
                     switch (addr) {
                         case 0xFF0F: b |= 0xE0; break; // IF returns 1 on first 3 unused bits
                         case 0xFF04:                //DIV on write = 0
                         case 0xFF44: b = 0; break;  //LY on write = 0
                         case 0xFF46: DMA(b); break;
-                        //case 0xFF00: b |= 0xC0; break;
+                            //case 0xFF00: b |= 0xC0; break;
                     }
                     //if (addr == 0xFF02 && b == 0x81) { //Temp Serial Link output for debug
-                       //Console.Write(Convert.ToChar(readByte(0xFF01)));
-                       //Console.ReadLine();
+                    //Console.Write(Convert.ToChar(readByte(0xFF01)));
+                    //Console.ReadLine();
                     //}
-                    IO[addr - 0xFF00] = b;
+                    IO[addr & 0x7F] = b;
                     break;
-                case ushort r when addr >= 0xFF80 && addr <= 0xFFFE:    // FF80-FFFE High RAM(HRAM)
-                    HRAM[addr - 0xFF80] = b;
-                    break;
-                case 0xFFFF: // FFFF Interrupt Enable Register.
-                    IE = b;
+                case ushort _ when addr <= 0xFFFF:    // FF80-FFFE High RAM(HRAM)
+                    HRAM[addr & 0x7F] = b;
                     break;
             }
         }
@@ -155,18 +168,22 @@ namespace ProjectDMG {
             writeByte(addr, (byte)w);
         }
 
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public byte bitSet(byte n, byte v) {
             return v |= (byte)(1 << n);
         }
 
-        public byte bitClear(byte n, byte v) {
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public byte bitClear(int n, byte v) {
             return v &= (byte)~(1 << n);
         }
 
-        public bool isBit(byte n, byte v) {
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public bool isBit(int n, int v) {
             return ((v >> n) & 1) == 1;
         }
 
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public void requestInterrupt(byte b) {
             IF = bitSet(b, IF);
         }
@@ -212,13 +229,7 @@ namespace ProjectDMG {
             gamePak.Init(rom);
         }
 
-        public void loadBootRom() {
-            byte[] rom = File.ReadAllBytes("DMG_ROM.bin");
-            Array.Copy(rom, 0, BOOT_ROM, 0, rom.Length);
-        }
-
     }
 }
-
 
 
