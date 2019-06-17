918334e78d9f6a6429b1505d41f0a0b28a39c75b
Blue <bluestorm.android@gmail.com>
2019-06-17 11:17:28 +0200

    Simplified switch statement

--- ProjectDMG/DMG/GamePak/MBC1.cs
+++ ProjectDMG/DMG/GamePak/MBC1.cs
@@ -18,7 +18,7 @@ namespace ProjectDMG.DMG.GamePak {
 
         public byte ReadERAM(ushort addr) {
             if (ERAM_ENABLED){
-                return ERAM[(ERAM_OFFSET * RAM_BANK) + addr];
+                return ERAM[(ERAM_OFFSET * RAM_BANK) + (addr & 0x1FFF)];
             } else {
                 return 0xFF;
             }
@@ -29,7 +29,7 @@ namespace ProjectDMG.DMG.GamePak {
         }
         
         public byte ReadHiROM(ushort addr) {
-            return ROM[(ROM_OFFSET * ROM_BANK) + addr];
+            return ROM[(ROM_OFFSET * ROM_BANK) + (addr & 0x3FFF)];
         }
 
         public void WriteERAM(ushort addr, byte value) {
@@ -40,15 +40,15 @@ namespace ProjectDMG.DMG.GamePak {
 
         public void WriteROM(ushort addr, byte value) {
             switch (addr) {
-                case ushort r when addr >= 0x0000 && addr <= 0x1FFF:
+                case ushort _ when addr < 0x2000:
                     ERAM_ENABLED = value == 0x0A ? true : false;
                     break;
-                case ushort r when addr >= 0x2000 && addr <= 0x3FFF:
+                case ushort _ when addr < 0x4000:
                     ROM_BANK = value & 0x1F; //only last 5bits are used
                     if (ROM_BANK == 0x00 || ROM_BANK == 0x20 || ROM_BANK == 0x40 || ROM_BANK == 0x60)
                         ROM_BANK++;
                     break;
-                case ushort r when addr >= 0x4000 && addr <= 0x5FFF:
+                case ushort _ when addr < 0x6000:
                     if(BANKING_MODE == 0) {
                         ROM_BANK |= value & 0x3;
                         if (ROM_BANK == 0x00 || ROM_BANK == 0x20 || ROM_BANK == 0x40 || ROM_BANK == 0x60)
@@ -57,13 +57,12 @@ namespace ProjectDMG.DMG.GamePak {
                         RAM_BANK = value & 0x3;
                     }
                     break;
-                case ushort r when addr >= 0x6000 && addr <= 0x7FFF:
+                case ushort _ when addr <= 0x8000:
                     // 00h = ROM Banking Mode (up to 8KByte RAM, 2MByte ROM) (default)
                     // 01h = RAM Banking Mode(up to 32KByte RAM, 512KByte ROM)
                     BANKING_MODE = value & 0x1;
                     break;
             }
         }
-
     }
 }
